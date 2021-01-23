e54e81be2364634f778e9c9d6a586cc0b3d08871
Ilaria Callegari <ilaria.callegari@gmail.com>
2021-01-23 12:37:41 +0000

    change jss of overview box and finish styling

--- src/components/OverviewBox.js
+++ src/components/OverviewBox.js
@@ -1,35 +1,62 @@
-import React, {memo} from "react";
+import React, { memo } from "react";
 import useStyles from "../styles/OverviewBoxStyle";
 import up from "../images/icon-up.svg";
 import down from "../images/icon-down.svg";
 
 function OverviewBox({ likes, views, logo, socialMedia }) {
   const classes = useStyles();
-  const { box, total} = classes;
+  const { box, total, imgLogo, arrow} = classes;
   return (
     <React.Fragment>
       <div className={box}>
-        <p>{likes.type}</p>
-        <img src={`${logo}`} alt={`${socialMedia} logo`} />
-        <p className={total}>{likes.tot}</p>
+        <div>
+          <p>{likes.type}</p>
+        </div>
+        <div>
+          <img className={imgLogo} src={`${logo}`} alt={`${socialMedia} logo`} />
+        </div>
+        <div>
+          <p className={total}>{likes.tot}</p>
+        </div>
         <div>
           <img
             src={likes.isUp ? `${up}` : `${down}`}
             alt={likes.isUp ? "up arrow" : "down arrow"}
+            className={arrow}
           />
-          <span style={{color: likes.isUp ? "hsl(163, 72%, 41%)" : "hsl(356, 69%, 56%)" }}>{likes.stat}</span>
+          <span
+            style={{
+              color: likes.isUp ? "hsl(163, 72%, 41%)" : "hsl(356, 69%, 56%)",
+            }}
+          >
+            {likes.stat}
+          </span>
         </div>
       </div>
+
       <div className={box}>
-        <p>{views.type}</p>
-        <img src={`${logo}`} alt={`${socialMedia} logo`} />
-        <p className={total}>{views.tot}</p>
+        <div>
+          <p>{views.type}</p>
+        </div>
+        <div>
+          <img className={imgLogo} src={`${logo}`} alt={`${socialMedia} logo`} />
+        </div>
+        <div>
+          <p className={total}>{views.tot}</p>
+        </div>
         <div>
           <img
             src={views.isUp ? `${up}` : `${down}`}
             alt={views.isUp ? "up arrow" : "down arrow"}
+            className={arrow}
           />
-          <span style={{color: views.isUp ? "hsl(163, 72%, 41%)" : "hsl(356, 69%, 56%)" }}>{views.stat}</span>
+          <span
+            style={{
+              color: views.isUp ? "hsl(163, 72%, 41%)" : "hsl(356, 69%, 56%)",
+            }}
+          >
+            {views.stat}
+          </span>
         </div>
       </div>
     </React.Fragment>
--- src/styles/OverviewBoxStyle.js
+++ src/styles/OverviewBoxStyle.js
@@ -10,21 +10,36 @@ const useStyles = createUseStyles({
     fontWeight: "700",
     flexWrap: "wrap",
     alignItems: "center",
+    borderRadius: 5,
     "&:hover": {
       backgroundColor: "hsl(228, 25%, 27%)",
     },
-    "& >*": {
+    "& > div": {
       width: "50%",
-      height: "50%"
+      height: "50%",
+      display: "flex",
+      alignItems: "center",
+      paddingRight: "2rem",
+      paddingLeft: "2rem",
+      "&:nth-child(1), &:nth-child(3)": {
+        justifyContent: "flex-start",
+      },
+      "&:nth-child(2), &:nth-child(4)": {
+        justifyContent: "flex-end",
+      },
     },
-    "& img": {
-      height: "1.25rem",
-      width: "1.25rem",
-    }
+  },
+  imgLogo: {
+    height: "1.25rem",
+    width: "1.25rem",
   },
   total: {
     fontSize: "2rem",
     color: "hsl(0, 0%, 100%)",
+  },
+  arrow: {
+    height: "0.25rem",
+    width: "0.5rem",
   },
 });
 
